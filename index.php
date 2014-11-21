76bc8b09c8e415060370e484ed94f29c0cd71b7f
Leonidas Kallipolitis <l.kallipolitis@atc.gr>
2014-11-21 17:40:22 +0200

    removed ga code

--- index.php
+++ index.php
@@ -57,22 +57,7 @@
         <script src="js/jqm.page.params.js"></script>
         <!-- Template specific functions and handlers -->    
         <script src="js/events-lib.js"></script>   
-        <script type="text/javascript">
-
-            var _gaq = _gaq || [];
-            _gaq.push(['_setAccount', 'UA-8323642-17']);
-            _gaq.push(['_trackPageview']);
-
-            (function() {
-                var ga = document.createElement('script');
-                ga.type = 'text/javascript';
-                ga.async = true;
-                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-                var s = document.getElementsByTagName('script')[0];
-                s.parentNode.insertBefore(ga, s);
-            })();
-
-        </script>
+        
 
     </head> 
 
